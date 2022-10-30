92f402fc086e70fb681fde2a1cda339eeabf9875
João Santos <joaosantos15@users.noreply.github.com>
2022-10-30 10:18:23 +0000

    Update GlacierDao.sol

--- contracts/GlacierDao.sol
+++ contracts/GlacierDao.sol
@@ -1,26 +1,24 @@
 pragma solidity ^0.8.7;
 
-contract DataDAO {
-
-    struct Epoch {
-        uint length;
-
-
-    }
+contract GlacierDao {
 
     struct Repo {
         string url;
-        string stored;
         string dealId;
         string pieceCid;
+        address spAddress;
         uint numberOfVotes;
+        bool stored;
         bool accepted;
+        uint yesVotes;
+        uint noVotes;
         bool isValue;
     }
 
     struct Member {
         address addr;
         uint availableVotes;
+        uint votingPower;
         bool isValue;
     }
 
@@ -29,48 +27,135 @@ contract DataDAO {
 
     mapping(address => Member) public daoMembers;
     mapping(string => Repo) public repos;
+    uint public currentEpoch;
+    
+    /*
+    epochStage
+    0 - Users fund DAO
+    1 - Repo submissions and voting, 
+    2 - SP submissions, 
+    3 - DAO vote to accept SP submissions,
+    4 - Epoch ungoing, SPs can start claiming rewards
+    5 - Epoch ends 
+    */
+ 
+    uint public epochStage; 
 
-    uint totalVotes;
+    uint public totalVotes;
+    address public admin;
 
     constructor() {
-       Member memory newMember = Member({addr: msg.sender, availableVotes: 0, isValue: true});
-       daoMembers[msg.sender] = newMember;
-       daoMembers.push(newMember);
+        admin = msg.sender;
+       //Member memory newMember = Member({addr: msg.sender, availableVotes: 0, isValue: true});
+       //daoMembers[msg.sender] = newMember;
+       //daoMembers.push(newMember);
    }
 
-   //function claim(string dealId, string pieceCID) public {}
-    
-    function isDealStillValid(string dealId) public return bool {
-        // filecoin magic 
-        // return filecoin.dealIsGood(dealId)
-        return false;
-        
-    }
+   // Admin functions
+   function adminAdvanceEpoch() public isAdmin{
+       currentEpoch+=1;
+   }
+
+   function adminAdvanceStage() public isAdmin{
+       epochStage+=1;
+   }
+
+   modifier isAdmin() {
+       require(msg.sender == admin);
+       _;
+   }
 
 
-    function voteForRepo(string memory url) public {
-        if(repos[url].isValue){
-            repos[url].numberOfVotes+= daoMembers[msg.sender].availableVotes;
-            if(repos[url].numberOfVotes > totalVotes/2){
-                repos[url].accepted = true;
-            }
-        }
-    }
-
-    
+    // Stage 0
     function joinDao() public payable  {
-        Member memory newMember = Member({addr: msg.sender, availableVotes: msg.value, isValue: true});
+        require(epochStage == 0,"Incorrect stage for joinDao, wait for next Epoch");
+        Member memory newMember = Member({addr: msg.sender, availableVotes: msg.value, votingPower: msg.value, isValue: true});
         totalVotes+=msg.value;
         daoMembers[msg.sender] = newMember;
     }
 
-    function leaveDao() public  isDaoMember{
+  /*  function leaveDao() public  isDaoMember{
             //address payable receiver = address(msg.sender)
             uint votes = daoMembers[msg.sender].availableVotes;
             payable(msg.sender).transfer(votes);
             totalVotes-=votes;
             delete daoMembers[msg.sender];
+    }*/
+
+    // Stage 1
+    function voteForRepo(string memory url) public isDaoMember {
+        require(epochStage == 1,"Incorrect stage for voteForRepo");
+        if(repos[url].isValue){
+            repos[url].numberOfVotes+= daoMembers[msg.sender].availableVotes;
+            
+            // for now, members cast all their votes in 1 repo
+            // this can be improved, with quadratic voting
+            daoMembers[msg.sender].availableVotes = 0;
+            if(repos[url].numberOfVotes > totalVotes/2){
+                repos[url].accepted = true;
+            }
+        } else {
+            Repo memory newRepo = Repo(
+                {
+                    numberOfVotes: daoMembers[msg.sender].availableVotes, 
+                    url: url, 
+                    isValue: true,
+                    dealId: "",
+                    pieceCid: "",
+                    spAddress:address(0),
+                    stored: false,
+                    accepted: false,
+                    yesVotes:0,
+                    noVotes:0
+                }
+            );
+            repos[url] = newRepo;
+        }
     }
+
+    // Stage 2
+    function minerSubmission (string memory url, string memory dealId, string memory pieceCid) public {
+        require(epochStage == 2,"Incorrect stage for minerSubmission");
+        Repo memory targetRepo = repos[url];
+        if(targetRepo.isValue){
+            targetRepo.dealId = dealId;
+            targetRepo.pieceCid = pieceCid;
+            targetRepo.stored = true;    
+            targetRepo.spAddress = msg.sender;
+        }
+    }
+
+    // Stage 3
+    function voteForMinerSubmission(string memory url, bool acceptSubmission) public isDaoMember {  
+        require(epochStage == 3,"Incorrect stage for voteForMinerSubmission");
+        if(repos[url].isValue){
+            if(acceptSubmission){
+                repos[url].yesVotes+= daoMembers[msg.sender].votingPower;
+            } else {
+                repos[url].noVotes+= daoMembers[msg.sender].votingPower;
+            }
+        }
+    }
+
+    // Stage 5
+    function claimReward(string memory url) public {
+        require(epochStage == 5,"Incorrect stage for claimReward");
+        Repo memory targetRepo = repos[url];
+        bool isDealValid = isDealStillValid(targetRepo.dealId, targetRepo.pieceCid);
+        require(isDealValid, "The deal is no longer valid");
+
+
+
+
+    }
+
+    function isDealStillValid(string memory dealId, string memory pieceCid) public returns (bool) {
+        // this is the part that needs to be linked to filecoin
+        return true;
+    }
+    
+    
+   
 
     modifier isDaoMember() {
         if(daoMembers[msg.sender].isValue){
